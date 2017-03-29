c88d50e09c57256e509795053421d7c38ad35a68
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-03-29 10:16:49 +0100

    entering players

--- spec/features/entering_players.rb
+++ spec/features/entering_players.rb
@@ -1,8 +1,10 @@
+require 'app'
+
 feature 'Testing player names' do
   scenario 'submitting names' do
     visit('/')
-    fill_in :player_1, with: 'Alfred'
-    fill_in :player_2, with: 'Pesto'
+    fill_in :Player_1, with: 'Alfred'
+    fill_in :Player_2, with: 'Pesto'
     click_button 'Submit'
     expect(page).to have_content("Alfred vs. Pesto")
  end
