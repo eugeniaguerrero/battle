1906eabfd2b57065f85fdf436aac31ecdd8b6158
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-03-29 11:27:47 +0100

    adds test helper file web_helpers to refactor

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,9 +1,13 @@
 feature 'Testing player names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1, with: 'Alfred'
-    fill_in :player_2, with: 'Pesto'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content("Alfred vs. Pesto")
  end
+end
+
+feature "Viewing hit points" do
+  scenario "viewing Player 2's hit points" do
+    sign_in_and_play
+    expect(page).to have_content("Pesto: 80HP")
+  end
 end
