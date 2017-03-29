252e04e17ab39c4704b2a41fe1682aa7c3d4fa08
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-03-29 18:16:02 +0100

    adds progress on skinny controllers

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,15 +2,27 @@ require 'game'
 
 describe Game do
 
-  subject(:game) {described_class.new}
-  let(:player_1) {double :player }
-  let(:player_2) {double :player }
+  subject(:game) {described_class.new('Alfred', 'Pesto')}
+  let(:alfred) {double :player }
+  let(:pesto) {double :player }
+
+  describe '#player_1_name' do
+    it 'tells us the name of player_1' do
+      expect(pesto.name).to eq 'Pesto'
+    end
+  end
+
+  describe '#player_1_score' do
+    it 'tells us the score or player_1' do
+    expect(pesto.score).to eq 100
+    end
+  end
 
 
   describe '#attack' do
     it 'damages the player' do
       expect(player_1).to receive(:reduce_score)
-      game.attack(player_1)
+      game.player_1_attacks
     end
   end
 
